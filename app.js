b724d1f71cd48a0dacee73507d6093fc6c63ba80
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-01-29 20:14:43 +0100

    cors errro fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 //Config
 const app = express();
 dotenv.config();
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 //Middlewar
 
 app.use(express.json());
