978f7b28f1331693c062535a6dd6b3ca2fa71051
BucoTEC <84572641+BucoTEC@users.noreply.github.com>
2022-02-04 15:43:35 +0100

    Update auth.js

--- midlewar/auth.js
+++ midlewar/auth.js
@@ -1,6 +1,6 @@
 const authenticated = (req, res, next) => {
   const { tajna } = req.query;
-  if (tajna === "04012000") {
+  if (tajna === "hahaha") {
     next();
   } else {
     res.send("You are not allowed to do that");
